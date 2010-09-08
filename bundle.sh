25422d018985e26f55b189e44dfcbc974aeb58d3
Rainux Luo <rainux@gmail.com>
2010-09-08 20:25:14 +0800

    Fixed bundle script

--- bundle.sh
+++ bundle.sh
@@ -2,10 +2,12 @@
 
 basename='twi-miku'
 zipball="$basename.bundled.`git rev-parse --short HEAD`.zip"
+echo Creating $zipball...
 rm -rf $zipball
 appcfg.rb update .
 rm -rf $basename
 mkdir $basename
-cp -r `git ls-files` WEB-INF/ $basename
-zip -r $zipball $basename
+git ls-files | cpio -pd --quiet $basename
+cp -r WEB-INF $basename
+zip -qr $zipball $basename
 rm -rf $basename
